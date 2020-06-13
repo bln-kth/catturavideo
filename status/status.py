696ad319f6ff1db4d3519b874fe2e2664f839b4d
Björn Larsson <bjornlarsson@bln-macbook.local>
2020-06-13 05:54:48 +0200

    update

---
+++ status/status.py
@@ -0,0 +1,26 @@
+import requests
+import sys
+import json
+import csv
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+f = open("cattura-units.csv")
+csv_f = csv.reader(f)
+
+for row in csv_f:
+	if not str(row[0]).startswith('#'):
+		ip = row[0]
+		user = row[1]
+		psw = row[2]
+		url = "https://"+ip+"/api/1/status/unit"
+		r = requests.get(url, auth=(user, psw), verify=False)
+		data=json.loads(r.text)
+		unitName=data['unitName']
+		machineName=data['machineName']
+		url = "https://"+cattura+"/api/status/capture"
+		r = requests.get(url, auth=(user, psw), verify=False)
+		data=json.loads(r.text)
+		state=data['state']
+		print ("unitName=",unitName,", machineName=",machineName,", state=",state,", ip=",cattura)
